0493b1901b4516d2b1f0978dec646c5135008285
Aditya Yadav <adityaydv12890@gmail.com>
2025-03-12 23:44:17 +0530

    Create Tree.cpp

---
+++ TREE/Tree.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* right, *left;
+
+    Node(int value) {
+        data = value;
+        right = left = NULL;
+    }
+};
+
+Node* binarytree() {
+    int x;
+    cin>>x;
+
+    if(x == -1) {
+        return NULL;
+    }
+
+    Node* temp = new Node(x);
+
+    // CREATE LEFT SIDE
+    temp->left = binarytree();
+
+    // CREATE RIGHT SIDE
+    temp->right = binarytree();
+
+    return temp;
+}
+
+void preorder(Node* root) {
+
+    if(root == NULL) {
+        return;
+    }
+
+    cout<<root->data;
+    preorder(root->left);
+    preorder(root->right);
+}
+
+int main() {
+    
+    cout<<"ENTER THE ROOT NDOE: ";
+    Node* root;
+    root = binarytree();
+
+    cout<<"PREORDER: ";
+    preorder(root);
+}
