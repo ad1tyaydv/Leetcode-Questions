96d6836a7e584092e8ed5ab81aad3ba755054333
Aditya Yadav <adityaydv12890@gmail.com>
2025-05-06 18:35:43 +0530

    Create Build Array from Permutation.cpp

---
+++ Array/Build Array from Permutation.cpp
@@ -0,0 +1,17 @@
+// Time Complexity - O(n)
+// Space Complexity - O(n)
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int> result(n);
+
+        for(int i=0;i<n;i++) {
+            result[i] = nums[nums[i]];
+        }
+
+        return result;
+    }
+};
