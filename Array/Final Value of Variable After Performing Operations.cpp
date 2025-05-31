8af61dfef28607af8fa5f73d8a175c0ff3b88dae
Aditya Yadav <adityaydv12890@gmail.com>
2025-05-31 21:26:42 +0530

    Create Final Value of Variable After Performing Operations.cpp

---
+++ Array/Final Value of Variable After Performing Operations.cpp
@@ -0,0 +1,22 @@
+// Time Complexity - O(1)
+// Space Complexity -O(1)
+
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+
+        int n = operations.size();
+
+        int result = 0;
+        for(int i=0;i<n;i++) {
+            if(operations[i] == "++X" || operations[i] == "X++") {
+                result++;
+            }
+            else {
+                result--;
+            }
+        }
+
+        return result;
+    }
+};
